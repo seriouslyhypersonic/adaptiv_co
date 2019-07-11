51478307ee1005eedefe035e0430875c61474c53
Nuno Alves de Sousa <nuno@uvic.ca>
2019-07-11 11:55:46 +0100

    Cmake: only compile platform-specific sources under the right platform

--- adaptivlib/src/utility/output/windows_console.cpp
+++ adaptivlib/src/utility/output/windows_console.cpp
@@ -11,18 +11,6 @@
 
 #include <windows.h>
 
-#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
-#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
-#endif
-
-#ifndef DISABLE_NEWLINE_AUTO_RETURN
-#define DISABLE_NEWLINE_AUTO_RETURN 0x0008
-#endif
-
-#ifndef ENABLE_VIRTUAL_TERMINAL_INPUT
-#define ENABLE_VIRTUAL_TERMINAL_INPUT 0x0200
-#endif
-
 #include <adaptiv/utility/output/windows_console.hpp>
 
 bool adaptiv::utility::output::windows::enableOutputSequences()
